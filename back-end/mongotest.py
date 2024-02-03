84be14c7bb8fd28d6e3b0ab31567fa867689d569
David <lairdavid1@gmail.com>
2024-02-03 14:54:59 -0500

    Mongo insertion

--- back-end/mongotest.py
+++ back-end/mongotest.py
@@ -1,15 +1,25 @@
 from pymongo import MongoClient
 
+class User:
+    def __init__(self, name, gender, age, email, password):
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.email = email
+        self.password = password
+    def __str__(self):
+        return f"{self.name}, {self.gender}, {self.age}, {self.email}"
 
 client = MongoClient("mongodb+srv://davidlai3:sdfghj45@cluster0.3eutqra.mongodb.net/?retryWrites=true&w=majority")
 db = client.Users
-
-post = {
-    "author": "David",
-    "text": "Testing testing 123"
-}
-
 info = db.UserInfo
-info_id = info.insert_one(post).inserted_id
-print(info_id)
+
+u1 = User("Jane", "F", 25, "janedoe@yahoo.com", "123456")
+
+def insert_user(user):
+    info.insert_one(user.__dict__)
+
+insert_user(u1)
+
+
 
