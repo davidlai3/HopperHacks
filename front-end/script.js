ab84127c88ad478dc36d9df29126c50ffbf86428
vivianyzhu103 <vivianyzhu010304@gmail.com>
2024-02-03 22:31:14 -0500

    changes

--- front-end/script.js
+++ front-end/script.js
@@ -1,3 +1,13 @@
 function toggleLogin() {
     document.querySelector(".overlay").classList.toggle("open");
   }
+
+  const forms = document.querySelector(".forms"),
+      links = document.querySelectorAll(".link");
+    
+links.forEach(link => {
+    link.addEventListener("click", e => {
+       e.preventDefault(); //preventing form submit
+       forms.classList.toggle("show-signup");
+    })
+})
